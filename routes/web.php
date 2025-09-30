29c7956cd029daa86459e2231f1d5a29b43b8bbc
Myo Min Thu <myominthu819@gmail.com>
2025-09-30 15:12:29 +0400

    updated

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Serve React app for all non-API routes
-Route::get('/{any}', function () {
-    $path = public_path('index.html');
-
-    if (!File::exists($path)) {
-        abort(404, 'Frontend not built yet');
-    }
-
-    return file_get_contents($path);
-})->where('any', '^(?!api/).*$');
+Route::get('/', function () {
+    return view('welcome');
+});
