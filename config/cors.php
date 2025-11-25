1ef154c1febeee5827662bc9b56111bc5830f08e
Myo Min Thu <myominthu819@gmail.com>
2025-11-25 12:47:19 +0400

    cors file updated

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,8 @@
 return [
     'paths' => ['api/v1/*', 'sanctum/csrf-cookie','storage/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['https://b2b.piueducation.org','http://localhost:5173'],
+    'allowed_origins' => ['https://b2b.piueducation.org', 'b2b.education.org', 'http://localhost:5173'],
+    'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
