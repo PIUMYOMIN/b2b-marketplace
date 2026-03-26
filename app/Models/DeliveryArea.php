ed3e0355c4b25dbf3977744b773ad300ad8ed60f
Myo Min Thu <myominthu819@gmail.com>
2026-03-27 02:14:01 +0400

    updated

--- app/Models/DeliveryArea.php
+++ app/Models/DeliveryArea.php
@@ -4,10 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Models\SellerProfile;
 
 class DeliveryArea extends Model
 {
     use SoftDeletes;
+
+    protected $table = 'seller_delivery_areas';
 
     protected $fillable = [
         'seller_profile_id',
